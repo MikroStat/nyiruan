34ffe8c75c7a602791b4e157c101d52a550428d2
MikroStat <alna.mikrostat@gmail.com>
2024-05-11 18:07:38 +0700

    Create sireum.js

---
+++ sireum.js
@@ -0,0 +1,18 @@
+function WinPopUpArea(iRasioTiLayar){  
+
+	// tinggi layar sesuai input //
+	let vLuhurna = iRasioTiLayar * screen.height.toFixed(0);
+	
+	// lebar layar sesuai input //
+	let vLegana  = .5 * iRasioTiLayar * screen.width.toFixed(0); 
+
+	// window centering //
+	let vTiLuhur     = (screen.height - vLuhurna) / 2;   
+	let vTiKenca     = (screen.width  - vLegana) / 2;
+
+	let kaTenjona = 'createnew=0, fullscreen=0, toolbar=yes, location=no, directories=0, status=1, ' +
+	'menubar=no, scrollbars=1, addressbar=no, titlebar=no, resizable=1, copyhistory=0, width=' +
+	vLegana + ', height=' + vLuhurna + ', top=' + vTiLuhur + ', left=' + vTiKenca;
+	
+	return kaTenjona;
+}
