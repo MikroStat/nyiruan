474a8585da4e047974ffc66a127acc7c4c1bfbd2
MikroStat <alna.mikrostat@gmail.com>
2025-04-23 21:37:42 +0700

    Update siraru.js

--- siraru.js
+++ siraru.js
@@ -51,6 +51,115 @@ function daftarPustaka(){
 		document.getElementById("alamatView").innerHTML = codeURL; 
 }
 
+function qUrlPilahKode(idEnt) {
+  // https:..../Penulis2015.png?rlkey=m.....fvh6x&dl=0
+  // ----0-----                ---------1-------------
+
+  let linkInp = idEnt;
+  let pilah = [];
+  let titik = [];  
+  titik[0] = linkInp.indexOf('fi/', 0)+3;
+  titik[1] = 21;
+  titik[2] = linkInp.indexOf('png?', 0)+4;
+  titik[3] = linkInp.indexOf('st=', 0);
+  
+  pilah[0] = linkInp.substr(titik[0], titik[1]);
+  pilah[1] = linkInp.substring(titik[0]+titik[1]+1, titik[2]-5);
+  pilah[2] = linkInp.substring(titik[2]+6, titik[3]-1);
+  pilah[3] = linkInp.slice(titik[3]+3, titik[3]+11);  
+  
+  return pilah; 
+}
+
+function qsetIdCodeForDirectLink(idEnt) {
+    let idInp = qUrlPilahKode(idEnt);
+    let tandapemisah = ' ';
+	// hanya untuk kerapian
+	let kodeKuota = 14;
+	let kodeBukuPj = idInp[1].length;
+	let tandaSpasi = '&nbsp;';
+	let spasi = '';
+	if (kodeBukuPj < kodeKuota) spasi = tandaSpasi.repeat(kodeKuota-kodeBukuPj);
+	
+	let kodeUnik = '&lt;p id="' + idInp[1] + '"' + spasi + '>' + idInp[0] + tandapemisah +
+	               idInp[2] + tandapemisah + idInp[3] + '&lt;/p&gt;';
+	
+   // document.getElementById('demo').innerHTML = kodeUnik; 
+	return kodeUnik;
+}
+
+function getTahunTerbitDiSitasi(idLinkEnt){
+	let tahunIndexSimbolAwal = '(';
+	let tahunIndexSimbolAkhir = ')';
+	let spEfek = 1;
+	
+	// deteksi tanda ( pertama 
+	let tahunIndexAwal  = idLinkEnt.indexOf(tahunIndexSimbolAwal);
+	let tahunIndexAkhir = idLinkEnt.indexOf(tahunIndexSimbolAkhir);
+	
+	// jumlah angka tahun tidak selalu 4 karena ada tambahan a, b, c dst.
+	let tahunJmlAngka = tahunIndexAkhir - tahunIndexAwal - spEfek;
+	let tahunTerbit = idLinkEnt.substr(tahunIndexAwal + spEfek, tahunJmlAngka);
+	
+	return tahunTerbit;
+}
+
+function getNamaPenulisDiSitasi(idLinkEnt){
+	let tahunIndexSimbolAwal = '(';
+	
+	let tahunIndexAwal = idLinkEnt.indexOf(tahunIndexSimbolAwal)-1;
+   let penulisDan = idLinkEnt.substr(0, tahunIndexAwal);
+	let penulis = penulisDan.replaceAll(/&/g,'&amp;' + 'amp;');
+	
+	return penulis;
+}
+
+function getPenerbit(idEnt) {
+	let titikPenerbitAwal = idEnt.lastIndexOf(".")+1;
+	let penerbit = idEnt.slice(titikPenerbitAwal);
+	penerbit = penerbit.replaceAll(/&/g,'&amp;' + 'amp;');
+	
+	return penerbit;
+}
+
+function hapusTitikDiAkhirSitasi(idEnt) {
+	// keharusan input tanpa tanda . di akhir sitasi awal
+	
+	let idEntPush = '';
+	let panjang = idEnt.length-1;
+	let code = idEnt.charCodeAt(panjang);
+	if(code == 46) {
+		idEntPush = idEnt.substring(0, panjang);
+		idEnt = idEntPush;
+	}	
+	return idEnt;
+	
+}
+
+function qKodeBukuStd(idEnt){
+  // let chInp = document.getElementById(idLink).innerHTML;
+  // let bukuKode = qKodeBukuAmbilDariUrlDb(idEnt);
+  let bukuKode = idEnt;
+  let kodeKuota = 14;
+  
+  // cek char terakhir (angka atau huruf)
+  let kodeJml = bukuKode.length;
+  let kodeAkhir = bukuKode.charCodeAt(kodeJml-1);
+  let kodeTahunJml = 4;
+  // 57 charCode angka 9 
+  if (kodeAkhir > 57) kodeTahunJml = 5;
+  let tahun = bukuKode.substr(kodeJml-kodeTahunJml, kodeTahunJml);
+  let penulis = bukuKode.substr(0, kodeJml-kodeTahunJml);
+  let bukuKodeStd = bukuKode;
+
+	if (kodeJml > kodeKuota) {
+		penulis = bukuKode.substr(0, 14-kodeTahunJml);
+		bukuKodeStd = penulis + tahun;
+	}
+	//  document.getElementById('demo').innerHTML = bukuKodeStd; 
+  return bukuKodeStd;  
+}
+
 function getNamaBaniDiSitasi(idEnt) {
 	let penulis = getNamaPenulisDiSitasi(idEnt);
 	let bani = penulis;
@@ -68,7 +177,7 @@ function getNamaBaniDiSitasi(idEnt) {
 function getJudulDiSitasi(idEnt) {
 
 	let titikMulai = idEnt.indexOf(')', 0) + 2;
-	let titikAkhir = idEnt.indexOf('.', titikMulai);
+	let titikAkhir = idEnt.lastIndexOf('.')+1;
 	let judulLengkap = idEnt.substring(titikMulai, titikAkhir);
 	let judul = ['', '', ''];
 	let judulSplit = [];	
@@ -97,6 +206,23 @@ function getJudulDiSitasi(idEnt) {
 			judul[1] = judulLengkap;
 			judul[2] = buttonTutup;
 	}
-	
+
 	return judul;
+}
+
+
+function eBookPdf (baniInp, tahunInp, judul0, judul1, judul2) {
+    
+	const butTanda = '&lt;/button&gt;';
+	let judul = '';
+	let titikTanda = 0;
+	
+	if( (judul0 == '') & (judul2 == butTanda)) {
+			judul = hapusTitikDiAkhirSitasi(judul1);
+	} else {
+			titikTanda = judul1.indexOf(butTanda, 0);
+			judul = judul1.substring(0, titikTanda);;
+	}
+	
+	return baniInp + ' (' + tahunInp + ') ' + judul;
 }
